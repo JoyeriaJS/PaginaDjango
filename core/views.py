642759ec921cb2ed2d54eb92adfeac3cf38010ef
unknown <dante.h@hotmail.es>
2025-11-28 11:25:49 -0300

    cambiosdisenoydarkbuton4

--- core/views.py
+++ core/views.py
@@ -23,6 +23,7 @@ from catalog.models import Order, OrderItem
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
+
 try:
     from weasyprint import HTML
 except Exception:
