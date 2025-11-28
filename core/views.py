4b3f7842d45da6317a24bf0da8dbb81f24cd8f07
unknown <dante.h@hotmail.es>
2025-11-28 11:19:42 -0300

    cambiosdisenoydarkbuton2

--- core/views.py
+++ core/views.py
@@ -34,13 +34,19 @@ except Exception:
 
 
 def home(request):
-    categories = Category.objects.annotate(n=Count('products')).order_by('-n','name')[:8]
-    latest_products = Product.objects.filter(is_active=True).order_by('-created_at')[:8]
+    banners_hero = Banner.objects.filter(
+        position=Banner.HOME_HERO,
+        is_active=True
+    ).order_by("order")
+
+    banners_strip = Banner.objects.filter(
+        position=Banner.HOME_STRIP,
+        is_active=True
+    ).order_by("order")
+
     return render(request, "core/home.html", {
-        "categories": categories,
-        "latest_products": latest_products,
-        "banners_hero": Banner.objects.filter(is_active=True, position=Banner.HOME_HERO).order_by('order','-updated_at')[:6],
-        "banners_strip": Banner.objects.filter(is_active=True, position=Banner.HOME_STRIP).order_by('order','-updated_at')[:6],
+        "banners_hero": banners_hero,
+        "banners_strip": banners_strip,
     })
 
 
