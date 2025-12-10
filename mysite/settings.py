c0355d80293b554c5c302a74aedfc551f1e6e28b
unknown <dante.h@hotmail.es>
2025-12-10 10:56:53 -0300

    cambiosmenucategories33

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ load_dotenv(BASE_DIR / ".env")  # opcional para entorno local
 
 # --- Seguridad / Debug -------------------------------------------------------
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_IPS = [
