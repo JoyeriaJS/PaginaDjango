783341182be41b6d53b77bcc39359304201d4993
unknown <dante.h@hotmail.es>
2025-12-19 13:16:12 -0300

    cambioscheckoutstockveri16

--- templates/core/templatetags/form_extras.py
+++ templates/core/templatetags/form_extras.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter(name="add_class")
-def add_class(field, css):
-    return field.as_widget(attrs={"class": css})
+def add_class(field, class_name):
+    return field.as_widget(attrs={"class": class_name})
