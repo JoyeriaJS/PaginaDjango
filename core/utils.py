c14bfc3ff0d8c2b869f7f67dff35e678da8eb6fd
unknown <dante.h@hotmail.es>
2025-12-23 12:44:38 -0300

    cambioswebhook20

--- core/utils.py
+++ core/utils.py
@@ -1,16 +1,20 @@
 import json
-from django.conf import settings
 from pathlib import Path
+from django.conf import settings
 
 def cargar_regiones_comunas():
     """
-    Carga el JSON que contiene todas las regiones,
-    ciudades y comunas de Chile.
+    Carga el JSON con regiones y comunas desde core/data/
     """
-    ruta = Path(settings.BASE_DIR) / "core" / "static" / "core" / "chile_regiones.json"
+    ruta = Path(settings.BASE_DIR) / "core" / "data" / "chile_regiones_comunas.json"
 
     try:
-        with open(ruta, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
+        if ruta.exists():
+            with open(ruta, "r", encoding="utf-8") as f:
+                return json.load(f)
+        else:
+            print("⚠️ Archivo JSON NO encontrado en:", ruta)
+    except Exception as e:
+        print("⚠️ Error leyendo JSON:", e)
+
+    return {}  # fallback
